fc8bdfe04274e6ede67260ad1510a3cb2ccdacb1
amoblin <amoblin@38427a0e-ebd1-11de-a198-37193048d1ed>
2010-09-17 08:22:17 +0000

    add shortcut.ini

--- winc/quickrun/shortcut.cpp
+++ winc/quickrun/shortcut.cpp
@@ -9,6 +9,7 @@
 #include <shellapi.h>
 
 #define bool BOOL
+#define MAXLENGTH 1024
 
 DWORD   g_main_tid = 0;
 HHOOK   g_kb_hook   = 0;
@@ -19,30 +20,105 @@ bool CALLBACK con_handler (DWORD)
         return TRUE;
 };
 
+int getvkcode(char *ch)
+{
+    if (!strcmp(ch,";"))
+        return 0x00ba;
+    else if (!strcmp(ch,"'"))
+        return 0x00de;
+    else if (!strcmp(ch,","))
+        return 0x00bc;
+    else if (!strcmp(ch,"."))
+        return 0x00be;
+    else if (!strcmp(ch,"/"))
+        return 0x00bf;
+    else if (!strcmp(ch,"a"))
+        return 0x0041;
+    else if (!strcmp(ch,"F1"))
+        return 0x0070;
+    else if (!strcmp(ch,"F2"))
+        return 0x0071;
+    else if (!strcmp(ch,"F3"))
+        return 0x0072;
+    else if (!strcmp(ch,"F4"))
+        return 0x0073;
+    else if (!strcmp(ch,"F5"))
+        return 0x0074;
+    else if (!strcmp(ch,"F6"))
+        return 0x0075;
+    else if (!strcmp(ch,"F7"))
+        return 0x0076;
+    else if (!strcmp(ch,"F8"))
+        return 0x0077;
+    else if (!strcmp(ch,"F9"))
+        return 0x0078;
+    else if (!strcmp(ch,"F10"))
+        return 0x0079;
+    else if (!strcmp(ch,"F11"))
+        return 0x007a;
+    else if (!strcmp(ch,"F12"))
+        return 0x007b;
+    else
+        return 0x0000;
+}
+
+int GetAction(PKBDLLHOOKSTRUCT p)
+{
+    FILE *fp;
+    fp = fopen("shortcut.ini","r");
+    if (fp == NULL) {
+        printf("Cannot open this file.\n");
+        return 0;
+    }
+    char buf[MAXLENGTH];
+    char KeyboardCode[100];
+    char CommandPath[100];
+    while(fgets(buf,MAXLENGTH,fp)) {
+        bool flag = 1;
+        char *ptr = strchr(buf,'=');
+        if(ptr) {
+            memset(KeyboardCode,0,strlen(KeyboardCode));
+            strncpy(KeyboardCode,buf,ptr-buf);
+            printf("compare with %s:",KeyboardCode);
+            char *token = strtok(KeyboardCode,"+");
+            while(token != NULL) {
+                printf("the key is: %s\n",token);
+                if(!strcmp(token,"ALT")) {
+                    flag = 0;
+                    if ((p->flags & LLKHF_ALTDOWN)!=0)
+                        flag = 1;
+                } else if (p->vkCode == getvkcode(token) && flag) {
+                        strcpy(CommandPath,ptr+sizeof(char));
+                        char *ptr = strchr(CommandPath,'\n');
+                        if(ptr) {
+                            *ptr = 0;
+                        }
+                        printf("execute %s:",CommandPath);
+                        ShellExecute(NULL,"open",CommandPath,"","", SW_SHOW );
+                    }
+                token = strtok(NULL,"+");
+            }
+        }
+    }
+    fclose(fp);
+    return 0;
+}
+
 LRESULT CALLBACK kb_proc (int code, WPARAM w, LPARAM l)
 {
         PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)l;
-        const char *info = NULL;
+        //const char *info = NULL;
         if (code == HC_ACTION) {
             switch(w) {
             case WM_KEYDOWN:
             case WM_SYSKEYDOWN:
-                if ( (p->vkCode == 0x00de) && ((p->flags & LLKHF_ALTDOWN)!=0) ) {
-                    ShellExecute(NULL,"open","D:\\MSYs\\mintty.bat","","", SW_SHOW );
-                } else if ( (p->vkCode == 0x00ba) && ((p->flags & LLKHF_ALTDOWN)!=0) ) {    //;
-                    ShellExecute(NULL,"open","C:\\Documents and Settings\\Administrator\\Local Settings\\Application Data\\Google\\Chrome\\Application\\chrome.exe","","", SW_SHOWMAXIMIZED );
-                } else if ( (p->vkCode == 0x00bc) && ((p->flags & LLKHF_ALTDOWN)!=0) ) {    //,
-                    ShellExecute(NULL,"open","explorer.exe","D:\\MSYS\\home\\Administrator\\received","", SW_SHOW );
-                } else if ( (p->vkCode == 0x00be) && ((p->flags & LLKHF_ALTDOWN)!=0) ) {    //.
-                }
+                    GetAction(p);
                     break;
             case WM_KEYUP:
             case WM_SYSKEYUP:
                     break;
             }
         }
-
-        // always call next hook
         return CallNextHookEx (g_kb_hook, code, w, l);
 };
 
