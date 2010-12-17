fce0eb7567ad0c903e6d1fb487a857d2d49380a8
amoblin <amoblin@ossxp>
2010-12-17 12:22:24 +0800

    增加迷你web服务器

---
+++ python/myweb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import web
+
+urls = ("/.*", "hello",)
+
+app = web.application(urls, globals())
+class hello:
+    def GET(self):
+        return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run()
+
