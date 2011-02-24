af2c7532d435624a0da1e724f5a62e89f125e739
amoblin <amoblin@ossxp>
2011-02-24 09:51:56 +0800

    使用脑图来记笔记

---
+++ notes/Linux Command.mm
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_114022856">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Linux
+    </p>
+  </body>
+</html>
+</richcontent>
+<node FOLDED="true" ID="ID_742073419" POSITION="right" 
+	TEXT="irssi">
+<node ID="ID_1902532499" 
+	TEXT="命令前要加斜扛"/>
+<node FOLDED="true" ID="ID_1898684688" 
+	TEXT="常用命令">
+<node ID="ID_378861502" 
+	TEXT="使用nick amoblin"/>
+</node>
+</node>
+<node ID="ID_1575144465" POSITION="right" 
+	TEXT="id"/>
+<node ID="ID_1785736586" POSITION="right" 
+	TEXT="ldd"/>
+<node ID="ID_1093539831" POSITION="right" 
+	TEXT="tcpdump -vvn"/>
+<node ID="ID_1202207542" POSITION="right" 
+	TEXT="arp -a"/>
+<node ID="ID_1378225186" POSITION="right" 
+	TEXT="netstat -rn"/>
+<node ID="ID_1039012473" POSITION="right" 
+	TEXT="update-alternatives --config python设定python版本"/>
+</node>
+</map>
---
+++ notes/Python.mm
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1750539614" 
+	TEXT="Python">
+<node FOLDED="true" ID="ID_1532989293" POSITION="right" 
+	TEXT="版本切换">
+<node ID="ID_376449127" 
+	TEXT="update-alternatives"/>
+</node>
+<node ID="ID_1235710951" POSITION="right" 
+	TEXT="dpkg -l python2.6显式安装状态"/>
+<node ID="ID_1387842951" POSITION="right" 
+	TEXT=""/>
+</node>
+</map>
