55f7a0531612e448dddd0afae227c6f1f84d9999
amoblin <amoblin@amoblin-laptop.(none)>
2011-04-23 17:40:40 +0800

    make backup dir in .vim

--- config/install.sh
+++ config/install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 ln _vimrc ~/.vimrc
+mkdir -p ~/.vim/backup
 
 ln _screenrc ~/.screenrc
 
 sudo ln gitconfig /etc
 
 sudo sh -c "tail -4 inputrc >> /etc/inputrc"
+
+git config --global user.name amoblin
+
+git config --global user.email amoblin@163.com
