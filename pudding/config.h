788f24d29398302e82c8f475ec28fb5964b408f8
amoblin <amoblin@163.com>
2011-06-29 10:09:52 +0800

    2.use fwrite and fread to access data.

---
+++ pudding/config.h
@@ -0,0 +1,7 @@
+#define SEN_LEN 18  //分词语句最大长度
+#define DATA_SIZE 1000  //学习数据的上限
+
+#define IN_NODES 36     //输入层节点数
+#define OUT_NODES 18     //输出层节点数
+#define HIDDEN_NODES 49   //隐含层节点数
+#define REAL_DATA_SIZE 100 //实际学习数据量
