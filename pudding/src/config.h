005008b8064f4cbfe36463306ba6e9ed74e73c06
amoblin <amoblin@gmail.com>
2011-08-15 21:01:15 +0800

    同时恢复使用make，以便在unix center服务器上运行

--- pudding/src/config.h
+++ pudding/src/config.h
@@ -1,13 +1,10 @@
-#define SEN_LEN 18  //分词语句最大长度,一个汉字占3个utf8字节
-#define UTF8_LEN 54
-#define UNI_LEN 36
+#define SEN_LEN 30  //分词语句最大长度,一个汉字占3个utf8字节
+#define UTF8_LEN 90
+#define UNI_LEN 60
 
 #define IN_NODES 8     //输入层节点数,一个汉字占2个unicode字节
 #define OUT_NODES 4     //输出层节点数,汉字的个数
 #define HIDDEN_NODES 48   //隐含层节点数
-
-#define REAL_DATA_SIZE 6 //实际学习数据量.    TODO: 动态分配
-
 
 #define DEBUG 1
 
