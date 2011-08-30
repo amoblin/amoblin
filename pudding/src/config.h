fe4633efdf72d57f1075b5f8427c019b18c5229d
amoblin <amoblin@gmail.com>
2011-08-31 00:53:17 +0800

    学习vim中statusline配置

--- pudding/src/config.h
+++ pudding/src/config.h
@@ -2,9 +2,10 @@
 #define UTF8_LEN 90
 #define UNI_LEN 60
 
-#define IN_NODES 8     //输入层节点数,一个汉字占2个unicode字节
+#define IN_LEN 8     //输入层节点数,一个汉字占2个unicode字节
+#define IN_NODES 64
 #define OUT_NODES 4     //输出层节点数,汉字的个数
-#define HIDDEN_NODES 48   //隐含层节点数
+#define HIDDEN_NODES 32   //隐含层节点数
 
 #define DEBUG 1
 
