441b9529b68c782d2ac1fdb4bda7801aa84b443e
amoblin <amoblin@gmail.com>
2011-09-25 11:24:46 +0800

    update sample.py

--- python/sample.py
+++ python/sample.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-print u"你好,世界"
+import cmd
+from optparse import OptionParser
+
+class cli_ui(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.intro="Welcome to amoblin's cli ui interface."
+        self.prompt="cli_ui> "
+
+    def help_exit(self):
+        print("no more help.")
+
+    def do_exit(self, line):
+        exit()
+
+if __name__ == "__main__":
+    cli = cli_ui()
+    cli.cmdloop()
