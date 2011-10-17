f20cdc00ba0e361302fc7c05639231d59b4ad2cf
amoblin <amoblin@cq40>
2011-10-17 14:30:53 +0800

    整理c例子

---
+++ splash/kmp.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+int pmatch(char *string, char *pat) {
+    int
--- splash/sample.cc
+++ splash/sample.cc
@@ -3,8 +3,12 @@ using namespace std;
 
 class Base {
     public:
+        /*
         Base() {}
         Base(int c):count(c){}
+        */
+
+        /* 虚函数 */
         virtual void print() const=0;
     private:
         int count;
@@ -15,10 +19,15 @@ class B {
 
 class Derived: public Base {
     public:
+        /*
         Derived():Base(0) {}
         Derived(int c):Base(c) {}
+        */
+
+        /* 多态性的体现. 前提是公有派生. */
         void print() const {cout<<"Derived"<<endl;}
     private:
+        /* 静态数据成员 */
         static B m_flag;
 };
 
@@ -35,8 +44,13 @@ int main()
 
     B b1;
 
+    Base b;
     Derived d(10);
+
+    /* 动态联编: 基对象指针调用虚函数 */
     Base *pb = &d;
+    pb->print();
+
     Base &cb = d;
 //    Derived dd = *pb;
 //    Derived &cd = cb;
