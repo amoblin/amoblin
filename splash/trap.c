083f1576981dba2f12d7f5320d5665d94dc744b9
amoblin <amoblin@cq40>
2011-10-19 10:57:42 +0800

    trap.c: 增加字符串学习

--- splash/trap.c
+++ splash/trap.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void set_string(char str[]) {
     printf("now in function:\n");
@@ -8,13 +9,27 @@ void set_string(char str[]) {
     printf("strlen str is %d\n", strlen(str));
 }
 
-int main()
-{
-    /******************* sizeof 和 strlen , 应用到指针上, 形参上 *****************/
+void sizeof_and_strlen() {
+    /* 随机串 */
+    char blank[10];
+    printf("%s\n", blank);
+
+    /* 溢出串 */
+    char a[3] = "abc";
+    printf("%s\n", a);
+    printf("%d\n", sizeof(a));
+    printf("%d\n", strlen(a));
+
+    /* 零串 */
+    char zero[10] = {0};
+    int i=0;
+    for(; i<10; i++) {
+        printf("%d", zero[i]);
+    }
+    printf("\n");
 
     char str[] = "123456789abcdef";
-    printf("init: char str[] = %s\n", str);
-    /* 数组首地址, sizeof统计实际占用的字节数,包括'\0\' */
+    /* 数组首地址, sizeof统计实际占用的字节数,包括'\0' */
     printf("size of str is %d\n", sizeof(str));
     printf("strlen str is %d\n", strlen(str));
 
@@ -26,18 +41,35 @@ int main()
     printf("strlen p is %d\n", strlen(p));
 
     set_string(str);
+}
+
+void memory_copy() {
+    char *str =(char*) malloc(3);
+    char *tmp = "ab";
+    printf("%s\n", str);
+    memset(str, 0, strlen(str));
+    printf("%s\n", str);
+    memcpy(str, tmp, strlen(str));
+    printf("%s\n", str);
+
+    memset(str, 0, strlen(str)+1);
+    printf("%s\n", str);
+    strncpy(str, tmp, strlen(str)+1);
+    printf("%s\n", str);
+
+    int test[2];
+    memset(test, 0, sizeof(test) * sizeof(int));
+    printf("%d\n", test[0]);
+}
+
+int main()
+{
+    /******************* sizeof 和 strlen , 应用到指针上, 形参上 *****************/
+    sizeof_and_strlen();
 
     /******************** 内存拷贝 ***************************************/
-    char *q = malloc(sizeof(str));
-    printf("%s\n", q);
-    memset(q, 0, sizeof(str));
-    printf("%s\n", q);
-    memcpy(q, p, sizeof(str));
-    printf("%s\n", q);
+    memory_copy();
 
-    memset(q, 0, strlen(str)+1);
-    printf("%s\n", q);
-    strncpy(q, p, strlen(str)+1);
-    printf("%s\n", q);
+    /* 运算符优先级 */
     return 0;
 }
